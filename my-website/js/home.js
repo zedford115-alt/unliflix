210fae78d3c2d2f3402ce7f3440eefb5b6f688ea
zedford115-alt <zedford115@gmail.com>
2025-08-28 00:45:05 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '11b0d0662f32f57e1c3644daf5badec4';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
@@ -119,5 +119,6 @@ const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
       displayList(tvShows, 'tvshows-list');
       displayList(anime, 'anime-list');
     }
+
 
     init();
